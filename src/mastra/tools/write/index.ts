d82dce067fbc46eddf7a5250a376e633ee598c61
Akshat <akshatagrawal8@gmail.com>
2026-04-07 09:02:05 +0530

    Remove createSignal export from index: clean up unused tool

--- src/mastra/tools/write/index.ts
+++ src/mastra/tools/write/index.ts
@@ -1,6 +1,5 @@
 export { createActionItem } from "./create-action-item.js";
 export { updateActionItemStage } from "./update-action-item-stage.js";
-export { createSignal } from "./create-signal.js";
 export { dismissSignal } from "./dismiss-signal.js";
 export { updateCompanyStage } from "./update-company-stage.js";
 export { assignKeyRole } from "./assign-key-role.js";
